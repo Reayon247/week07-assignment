6184be251f216143b8ce2e79c0605b21085df0f6
Ryan Nugent <ryan.nugent247@gmail.com>
2025-06-13 10:25:02 +0100

    feat: table added that collects the gametracker data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,31 @@ import Home from "./Pages/Home";
 import GameTracker from "./Pages/GameTracker";
 import NotFound from "./Pages/NotFound";
 import Header from "./Components/Header";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    async function getGames() {
+      const response = await fetch(`${import.meta.env.VITE_READ_URL}`);
+      const data = await response.json();
+      setGames(data);
+      console.log(data);
+    }
+    getGames();
+  }, []);
+
   return (
     <>
       <Header />
       <Routes>
         <Route path={"/*"} element={<NotFound />} />
-        <Route path={"/"} element={<Home />} />
-        <Route path={"/gametracker"} element={<GameTracker />} />
+        <Route path={"/"} element={<Home gameData={games} />} />
+        <Route
+          path={"/gametracker"}
+          element={<GameTracker gameData={games} />}
+        />
       </Routes>
     </>
   );
--- client/src/Pages/GameTracker.jsx
+++ client/src/Pages/GameTracker.jsx
@@ -1,7 +1,10 @@
-export default function GameTracker() {
+import Table from "../Components/Table";
+
+export default function GameTracker(props) {
   return (
     <>
       <h2>Game Tracker</h2>
+      <Table gameData={props.gameData} />
     </>
   );
 }
