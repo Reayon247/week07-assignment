092d42d2f8e1c2fd26ed8a59cce59027fb18cf9c
Ryan Nugent <ryan.nugent247@gmail.com>
2025-06-14 16:56:42 +0100

    feat: added media queries to the home page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,29 +4,31 @@ export default function Home(props) {
   return (
     <main className="main">
       <h2>Home - Ongoing Games</h2>
-      {props.onGoingGames.map((game) => {
-        return (
-          <div className="ongoinggames" key={game.id}>
-            <img
-              src={game.thumbnail}
-              alt={game.game_name}
-              width={175}
-              className="img"
-            />
-            <h3 className="gamename">{game.game_name}</h3>
-            <p className="hours">
-              Hours Played:{" "}
-              <span className="highlight">
-                {game.hours ? game.hours : "Unknown"} Hrs{" "}
-              </span>
-            </p>
-            <p className="date">
-              {" "}
-              Date Started: <span className="highlight">{game.started}</span>
-            </p>
-          </div>
-        );
-      })}
+      <div className="ongoinggames-container">
+        {props.onGoingGames.map((game) => {
+          return (
+            <div className="ongoinggames" key={game.id}>
+              <img
+                src={game.thumbnail}
+                alt={game.game_name}
+                width={175}
+                className="img"
+              />
+              <h3 className="gamename">{game.game_name}</h3>
+              <p className="hours">
+                Hours Played:{" "}
+                <span className="highlight">
+                  {game.hours ? game.hours : "Unknown"} Hrs{" "}
+                </span>
+              </p>
+              <p className="date">
+                {" "}
+                Date Started: <span className="highlight">{game.started}</span>
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
